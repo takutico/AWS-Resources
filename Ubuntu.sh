05978bb95b04523e62e35ad6ef005e54ba05a84a
typ <takutico@users.noreply.github.com>
2017-03-23 17:38:10 +1100

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install mysql-server mysql-client
 # Install PHP
 sudo apt-get -y install php libapache2-mod-php php-mcrypt php-mysql
 # Test PHP
-echo "<?php\nphpinfo();\n?>" > sudo /var/www/html/info.php;
+echo "<?php\nphpinfo();\n?>" > sudo /var/www/html/info.php
 # Install phpmyadmin
 sudo apt-get -y install phpmyadmin
 sudo ln -s /usr/share/phpmyadmin /var/www/html/
