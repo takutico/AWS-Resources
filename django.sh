b71321fe0ae540deab84fd498becbd8d020850d8
typ <takutico@users.noreply.github.com>
2017-03-27 14:15:37 +1100

    Update django.sh

--- django.sh
+++ django.sh
@@ -4,7 +4,8 @@ sudo apt-get install python3-pip libapache2-mod-wsgi-py3
 # Once we have pip installed we install virtualenv
 sudo pip3 install virtualenv
 # if you see locale error
-# export LC_ALL=en_US.UTF-8
+# export LANG='en_US.UTF-8'
+# export LC_ALL='en_US.UTF-8'
 
 # If you want to use mysqlclient
 # prerequisites
